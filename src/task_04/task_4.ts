f5a5c11c1afe63782ed969e6c27fd46bf4b340d5
Anton Rokunets <9251580262@mail.ru>
2022-02-03 18:42:43 +0300

    types

---
+++ src/task_04/task_4.ts
@@ -0,0 +1,43 @@
+type FIXME = {
+    state: "initial" | "inWork";
+} | null;
+
+type Order =
+    | {
+    state: "initial";
+    sum: number;
+}
+    | {
+    state: "inWork";
+    sum: number;
+    workerId: number;
+}
+    | {
+    state: "buyingSupplies";
+    sum: number;
+    workerId: number;
+    suppliesSum: number;
+}
+    | {
+    state: "producing";
+    sum: number;
+    workerId: number;
+    suppliesSum: number;
+    produceEstimate: Date;
+}
+    | {
+    state: "fulfilled";
+    sum: number;
+    workerId: number;
+    suppliesSum: number;
+    produceEstimate: Date;
+    fulfillmentDate: Date;
+};
+
+export const filterOnlyInitialAndInWorkOrder = (order: Order): FIXME => {
+    if (order.state === "initial" || order.state === "inWork") {
+        return order;
+    }
+
+    return null;
+};
---
+++ src/task_05/task_05.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+type FIXME = any;
+
+
+// Hint: infer
+export const getDefaultProps = <T>(
+    component: React.ComponentType<T>
+): FIXME => {
+    return component.defaultProps;
+};
